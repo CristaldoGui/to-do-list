6c4fe0a118945e42cbdaeedabb51a11670a961af
guilherme <guilherme.cristaldo15@gmail.com>
2024-09-18 18:06:02 -0400

    melhoria nas funções do arquivo script.js

--- client/js/script.js
+++ client/js/script.js
@@ -3,35 +3,43 @@ const inputTask = document.getElementById('input-task');
 const form = document.getElementById('form');
 const divInput = document.getElementById('div-input');
 
+function setAttributeElement(element, attr, value){
+    element.setAttribute(attr, value);
+}
+
+function addClass(element, classToAdd){
+    element.classList.add(classToAdd);
+}
+
 function createInputTask(text){
     //cria o elemento DIV e atribui uma classe a ele 
     let div = document.createElement('div');
-    div.setAttribute('onclick', 'taskCheked(this, event)');
-    div.classList.add('img-animation');
-    div.classList.add('div-task');
+    setAttributeElement(div, 'onclick', 'taskCheked(this, event)');
+    addClass(div, 'img-animation');
+    addClass(div, 'div-task');
 
     //cria o elemento IMG e seta seus atributos
     let img = document.createElement('img');
-    img.setAttribute('src', './images/unchecked.png');
-    img.setAttribute('width', '24px');
-    img.setAttribute('alt', 'Icon');
+    setAttributeElement(img, 'src', './images/unchecked.png');
+    setAttributeElement(img, 'width', '24px');
+    setAttributeElement(img, 'alt', 'Icon');
 
     //cria o elemento P e adiciona o seu texto
     let p = document.createElement('p');
-    p.classList.add('input-task-check');
+    addClass(p, 'input-task-check');
     text = document.createTextNode(text);
     p.appendChild(text);
 
     //cria o elemento INPUT e atribui seu value
-    let input = document.createElement('input');
-    input.setAttribute('type', 'submit');
-    input.setAttribute('onclick', 'excludeTask(this)')
-    input.setAttribute('value', 'x');
+    let buttonExclue = document.createElement('input');
+    setAttributeElement(buttonExclue, 'type', 'submit');
+    setAttributeElement(buttonExclue, 'onclick', 'excludeTask(this)');
+    setAttributeElement(buttonExclue, 'value', 'x');
 
     //colocando os elementos dentro dos outros
     div.appendChild(img);
     div.appendChild(p);
-    div.appendChild(input);
+    div.appendChild(buttonExclue);
     form.appendChild(div)
 }
 
@@ -39,15 +47,16 @@ function addTask(){
     //pega o texto do input e atribui a variavel task
     let task = inputTask.value;
 
+    //se a task estiver vazia não permite q ela seja adicionada e avisa o usuário
     if(task == ''){
         return alert('Você não pode adicioanar uma task vazia');
     }
 
-    //limpa o input apos a task ser adicionada 
-    inputTask.value = '';
-
     //chama a função q cria o HTML da task e passa o variavel task q contem o valor da task 
     createInputTask(task);
+
+    //limpa o input apos a task ser adicionada 
+    inputTask.value = '';
 }
 
 
